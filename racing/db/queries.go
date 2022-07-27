6e80420153e91d8601511a0fc59bb732f13a7a56
GazKendall <gaz.kendall@gmail.com>
2022-07-27 12:58:41 +1000

    Implemented a status response field for ListRaces that returns OPEN if the advertised race start time is in the future and CLOSED if in the past

--- racing/db/queries.go
+++ racing/db/queries.go
@@ -6,6 +6,9 @@ const (
 
 func getRaceQueries() map[string]string {
 	return map[string]string{
+		// status calculated field returns:
+		//   - 0 (OPEN) if advertised start time is >= the current date/time
+		//   - 1 (CLOSED) if advertised start is < the current date/time
 		racesList: `
 			SELECT 
 				id, 
@@ -13,7 +16,8 @@ func getRaceQueries() map[string]string {
 				name, 
 				number, 
 				visible, 
-				advertised_start_time 
+				advertised_start_time, 
+				CASE WHEN datetime(advertised_start_time, 'localtime') >= datetime('now','localtime') THEN 0 ELSE 1 END status 
 			FROM races
 		`,
 	}
